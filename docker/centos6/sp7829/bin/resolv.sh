8d9b6c4d992a31863d4a193d32f3ead234784014
Paul.Gilligan <Paul.Gilligan@moneysupermarket.com>
2014-11-11 18:07:31 +0000

    sp7829 WIP

---
+++ docker/centos6/sp7829/bin/resolv.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Simple script to switch to skydns in docker container.
+# 
+
+RESOLVCONF=/etc/resolv.conf
+
+SKYDNS=$(ping -c 1 skydns|grep "PING" | sed -E 's/PING .* .([0-9.]+). .*/\1/g') > /dev/null
+if [[ ! -z "${SKYDNS}" ]]; then
+  echo "Found skydns at ${SKYDNS}"
+  sed -e "s/\(nameserver\) .*/\1 ${SKYDNS}/" ${RESOLVCONF} > /tmp/resolv.conf
+  echo "nameserver 8.8.8.8" >> /tmp/resolv.conf
+  cp /tmp/resolv.conf ${RESOLVCONF}
+  chmod 644 ${RESOLVCONF}
+  chown root:root ${RESOLVCONF}
+else
+  echo "Nothing changed, was unable to ping skydns!"
+fi

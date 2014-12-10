d255746f2d4f54dc6c255caf002bde96efc7e55c
Paul.Gilligan <Paul.Gilligan@moneysupermarket.com>
2014-12-10 15:31:36 +0000

    add tango WIP

--- docker/mixin/vagrant.service.options.mixin.rb
+++ docker/mixin/vagrant.service.options.mixin.rb
@@ -13,6 +13,8 @@
 services.each do |service, params|
 
   if service == "#{TRUSTRAP_ENV}-#{TRUSTRAP_SERVICE}"
+
+    # domain
     domain = params.detect {|param| param['domain']}
     TRUSTRAP_DOMAIN = domain['domain']
     if TRUSTRAP_DOMAIN
@@ -21,21 +23,48 @@ services.each do |service, params|
       abort("Domain variable: 'domain' is not set in file #{YAML_OPTIONS}, exiting ...")
     end
 
+    fe_domain = params.detect {|param| param['fe-domain']}
+    TRUSTRAP_FE_DOMAIN = fe_domain['fe-domain']
+    if TRUSTRAP_FE_DOMAIN
+      puts "TRUSTRAP_FE_DOMAIN       #{TRUSTRAP_FE_DOMAIN}"
+    else
+      abort("Domain variable: 'fe-domain' is not set in file #{YAML_OPTIONS}, exiting ...")
+    end
+
+    be_domain = params.detect {|param| param['be-domain']}
+    TRUSTRAP_BE_DOMAIN = be_domain['be-domain']
+    if TRUSTRAP_BE_DOMAIN
+      puts "TRUSTRAP_BE_DOMAIN       #{TRUSTRAP_BE_DOMAIN}"
+    else
+      abort("Domain variable: 'be-domain' is not set in file #{YAML_OPTIONS}, exiting ...")
+    end
+
+    # dns
     skydns = params.detect {|param| param['skydns']}
     if skydns
       TRUSTRAP_WITH_SKYDNS = skydns['skydns']
     else
-     TRUSTRAP_WITH_SKYDNS = false
+      TRUSTRAP_WITH_SKYDNS = false
     end
     puts "TRUSTRAP_WITH_SKYDNS  #{TRUSTRAP_WITH_SKYDNS}"
 
+    # ca
     ejbca = params.detect {|param| param['ejbca']}
     if ejbca
       TRUSTRAP_WITH_EJBCA = ejbca['ejbca']
     else
-     TRUSTRAP_WITH_EJBCA = false
+      TRUSTRAP_WITH_EJBCA = false
     end
     puts "TRUSTRAP_WITH_EBJCA   #{TRUSTRAP_WITH_EJBCA}"
+
+    # haproxy gw's aka tango
+    tango = params.detect {|param| param['tango']}
+    if tango
+      TRUSTRAP_WITH_TANGO = tango['tango']
+    else
+      TRUSTRAP_WITH_TANGO = false
+    end
+    puts "TRUSTRAP_WITH_TANGO   #{TRUSTRAP_WITH_TANGO}"
 
   else
     abort("Service variable: #{TRUSTRAP_ENV}-#{TRUSTRAP_SERVICE} is not set in file #{YAML_OPTIONS}, exiting ...")
---
+++ docker/mixin/vagrant.service.tango.mixin.rb
@@ -0,0 +1,49 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+#
+# Vagrantfile   Provision MSM trustrap services.
+#
+# Authors:      Paul Gilligan, <Paul.Gilligan@moneysupermarket.com>
+#
+# Description:  Mixin ruby for provisioning Vagrantfile.
+
+# -----------------------------------------------------------------------------
+# Provision TANGO (dual haproxy gateways, one external on FE one internal on BE
+# -----------------------------------------------------------------------------
+TANGO_FE_FQDN = "#{TANGO_NAME}.#{TRUSTRAP_FE_DOMAIN}"
+TANGO_BE_FQDN = "#{TANGO_NAME}.#{TRUSTRAP_BE_DOMAIN}"
+config.vm.define "#{TANGO_FE_FQDN}" do |m|
+  m.vm.provider "docker" do |vm|
+    vm.name            = "#{TANGO_FE_FQDN}"
+    vm.image           = "pauldavidgilligan/docker-centos6-haproxy"
+    vm.has_ssh         = true
+    vm.create_args = ["--privileged", "--dns-search=#{TRUSTRAP_DOMAIN}", "--dns=8.8.8.8", "--hostname=#{TANGO_FE_FQDN}"]
+    vm.vagrant_machine = "dockerhost"
+    vm.vagrant_vagrantfile = "../../Vagrantfile.proxy"
+    if TRUSTRAP_WITH_SKYDNS
+      vm.link("#{SKYDNS_NAME}.#{TRUSTRAP_DOMAIN}:#{SKYDNS_NAME}")
+    end
+  end
+  if TRUSTRAP_WITH_SKYDNS
+    m.vm.provision :shell, :path => "bin/shell.sh", :args => "-n #{SKYDNS_NAME} -m resolv -d #{TRUSTRAP_DOMAIN}"
+  end
+end
+
+config.vm.define "#{TANGO_BE_FQDN}" do |m|
+  m.vm.provider "docker" do |vm|
+    vm.name            = "#{TANGO_BE_FQDN}"
+    vm.image           = "pauldavidgilligan/docker-centos6-haproxy"
+    vm.has_ssh         = true
+    vm.create_args = ["--privileged", "--dns-search=#{TRUSTRAP_DOMAIN}", "--dns=8.8.8.8", "--hostname=#{TANGO_BE_FQDN}"]
+    vm.vagrant_machine = "dockerhost"
+    vm.vagrant_vagrantfile = "../../Vagrantfile.proxy"
+    if TRUSTRAP_WITH_SKYDNS
+      vm.link("#{SKYDNS_NAME}.#{TRUSTRAP_DOMAIN}:#{SKYDNS_NAME}")
+    end
+  end
+  if TRUSTRAP_WITH_SKYDNS
+    m.vm.provision :shell, :path => "bin/shell.sh", :args => "-n #{SKYDNS_NAME} -m resolv -d #{TRUSTRAP_DOMAIN}"
+  end
+end
+
+

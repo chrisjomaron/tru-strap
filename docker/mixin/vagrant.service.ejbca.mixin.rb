60ce89655660328c7b86614cd0ded4df1a8f1a3a
Paul.Gilligan <Paul.Gilligan@moneysupermarket.com>
2014-12-10 15:36:23 +0000

    add tango WIP

--- docker/mixin/vagrant.service.ejbca.mixin.rb
+++ docker/mixin/vagrant.service.ejbca.mixin.rb
@@ -10,22 +10,22 @@
 # -----------------------------------------------------------------------------
 # Provision EJBCA
 # -----------------------------------------------------------------------------
-
+EJBCA_FQDN = "#{EJBCA_NAME}.#{TRUSTRAP_DOMAIN}"
 config.vm.network "forwarded_port", guest: 8080, host: 8080
 config.vm.network "forwarded_port", guest: 8443, host: 8443
 config.vm.network "forwarded_port", guest: 9990, host: 9990
 config.vm.network "forwarded_port", guest: 9999, host: 9999
-config.vm.define "#{EJBCA_NAME}" do |m|
+config.vm.define "#{EJBCA_FQDN}" do |m|
   m.vm.provider "docker" do |vm|
-    vm.name            = "#{EJBCA_NAME}.#{TRUSTRAP_DOMAIN}"
+    vm.name            = "#{EJBCA_FQDN}"
     vm.image           = "pauldavidgilligan/docker-centos6-ejbca-mysql"
     vm.has_ssh         = true
-    vm.create_args = ["--privileged", "--dns-search=#{TRUSTRAP_DOMAIN}", "--dns=8.8.8.8", "--hostname=#{EJBCA_NAME}"]
+    vm.create_args = ["--privileged", "--dns-search=#{TRUSTRAP_DOMAIN}", "--dns=8.8.8.8", "--hostname=#{EJBCA_FQDN}"]
     vm.vagrant_machine = "dockerhost"
     vm.vagrant_vagrantfile = "../../Vagrantfile.proxy"
   end
   m.vm.provision "shell", inline: "echo \"ant deploy(approx 1 min), wait to complete then ant install(approx 2mins)\""
-  m.vm.provision :shell, :path => "bin/shell.sh", :args => "-n #{SKYDNS_NAME} -m ejbca -d #{TRUSTRAP_DOMAIN}"
+  m.vm.provision :shell, :path => "bin/shell.sh", :args => "-n #{EJBCA_NAME} -m ejbca -d #{TRUSTRAP_DOMAIN}"
   if TRUSTRAP_WITH_SKYDNS
     m.vm.provision :shell, :path => "bin/shell.sh", :args => "-n #{SKYDNS_NAME} -m resolv -d #{TRUSTRAP_DOMAIN}"
   end

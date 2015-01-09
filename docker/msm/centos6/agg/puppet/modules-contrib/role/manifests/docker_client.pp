b404d6571034839369adabc0b539e9ebfc397f6d
Paul.Gilligan <Paul.Gilligan@moneysupermarket.com>
2015-01-09 16:53:15 +0000

    puppet WIP

--- docker/msm/centos6/agg/puppet/modules-contrib/role/manifests/docker_client.pp
+++ docker/msm/centos6/agg/puppet/modules-contrib/role/manifests/docker_client.pp
@@ -3,6 +3,23 @@ class role::docker_client {
   include ::profile::maven
   include ::profile::skydns_client
 
+  $alias_ca              = "root.msm.internal"
+  $path                  = "/etc/pki/msm"
+  $combined_password     = "8ruNestu"
+  $truststore_password   = "changeit"
+  $challenge_password    = "sW6Qecra"
+  $combined_algorithm    = "SHA256WithRSAEncryption"
+  $key_size              = "4096"
+  $cert_days             = "7"
+  $x509_cn               = "*.web.be.msm.internal"
+  $x509_ou               = "OPS"
+  $x509_o                = "Moneysupermarket.com Group PLC"
+  $x509_l                = "Chester"
+  $x509_s                = "Cheshire"
+  $x509_c                = "UK"
+  $ejbca_url             = "http://ejbca.msm.internal:8080/ejbca/publicweb/apply/scep/pkiclient.exe"
+  $ejbca_ca              = "MSMCA"
+
   # java -jar /opt/ife-toolbelt/ife-spring-shell/target/ife-spring-shell-1.1.4.one-jar.jar
 
   file { "/etc/pki/msm":
@@ -12,17 +29,22 @@ class role::docker_client {
     mode   => 777
   }
 
-  vcsrepo { '/opt/ife-toolbelt':
+  vcsrepo { "/opt/ife-toolbelt":
     ensure   => present,
     provider => git,
-    source   => 'https://github.com/pauldavidgilligan-msm/ife-toolbelt.git',
+    source   => "https://github.com/pauldavidgilligan-msm/ife-toolbelt.git",
     revision => '1.1.4',
   } ->
-  exec { "ife-toolbelt":
+  exec { "ife-toolbelt-mvn":
     cwd     => "/opt/ife-toolbelt",
     command => "mvn clean install",
     path    => "/usr/local/bin/:/usr/bin:/bin/",
     require => Class['Maven::Maven']
+  } ->
+  file { "ife-toolbelt-cfg":
+    path    => '/etc/ife-toolbelt.cfg',
+    ensure  => file,
+    content => template('role/ife-toolbelt/ife-toolbelt.cfg.erb')
   }
 
   if versioncmp($::puppetversion,'3.6.1') >= 0 {

19833608f65b1256839448490f71acd06b2f5066
Paul.Gilligan <Paul.Gilligan@moneysupermarket.com>
2015-01-09 15:41:52 +0000

    puppet WIP

--- docker/msm/centos6/agg/puppet/modules-contrib/role/manifests/docker_client.pp
+++ docker/msm/centos6/agg/puppet/modules-contrib/role/manifests/docker_client.pp
@@ -2,4 +2,27 @@ class role::docker_client {
   include ::profile::java
   include ::profile::maven
   include ::profile::skydns_client
+
+  # java -jar /opt/ife-toolbelt/ife-spring-shell/target/ife-spring-shell-1.1.4.one-jar.jar
+
+  file { "/etc/pki/msm":
+    ensure => "directory",
+    owner  => "root",
+    group  => "wheel",
+    mode   => 777
+  }
+
+  vcsrepo { '/opt/ife-toolbelt':
+    ensure   => present,
+    provider => git,
+    source   => 'https://github.com/pauldavidgilligan-msm/ife-toolbelt.git',
+    revision => '1.1.4',
+  } ->
+  exec { "ife-toolbelt":
+    cwd     => "/opt/ife-toolbelt",
+    command => "mvn clean install",
+    path    => "/usr/local/bin/:/usr/bin:/bin/",
+    require => Class['Maven::Maven']
+  }
+
 }

1deadcf100e4db8f005fa0ed39da728bf5623986
Paul.Gilligan <Paul.Gilligan@moneysupermarket.com>
2015-01-09 16:09:19 +0000

    puppet WIP

--- docker/msm/centos6/agg/puppet/modules-contrib/role/manifests/docker_client.pp
+++ docker/msm/centos6/agg/puppet/modules-contrib/role/manifests/docker_client.pp
@@ -25,4 +25,13 @@ class role::docker_client {
     require => Class['Maven::Maven']
   }
 
+  if versioncmp($::puppetversion,'3.6.1') >= 0 {
+
+    $allow_virtual_packages = hiera('allow_virtual_packages',false)
+
+    Package {
+      allow_virtual => $allow_virtual_packages,
+    }
+}
+
 }

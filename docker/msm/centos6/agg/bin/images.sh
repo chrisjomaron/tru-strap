79f5729eb68207cd901e8c5ac517fde1b42dd9af
Paul.Gilligan <Paul.Gilligan@moneysupermarket.com>
2015-01-20 13:49:37 +0000

    docker pull

--- docker/msm/centos6/agg/bin/images.sh
+++ docker/msm/centos6/agg/bin/images.sh
@@ -1,4 +1,4 @@
-docker pull registry1-eu1.moneysupermarket.com:5000/go-skydns:latest
+docker pull pauldavidgilligan/go-skydns:latest
 docker pull registry1-eu1.moneysupermarket.com:5000/docker-centos6-base-image:latest
 docker pull registry1-eu1.moneysupermarket.com:5000/docker-centos6-puppet-ruby215:latest
 docker pull registry1-eu1.moneysupermarket.com:5000/docker-centos6-ejbca-mysql:latest
